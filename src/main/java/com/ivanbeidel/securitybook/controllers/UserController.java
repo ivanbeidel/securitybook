fb9bf0359f624c9147bd97f848457cfc5ce31e8d
ivanbeidel <ivanbeidel@gmail.com>
2024-09-11 00:07:42 +0200

    add admin and user controller

---
+++ src/main/java/com/ivanbeidel/securitybook/controllers/UserController.java
@@ -0,0 +1,17 @@
+package com.ivanbeidel.securitybook.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+    @GetMapping
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("Ok");
+    }
+}
